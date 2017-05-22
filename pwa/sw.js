a86c3351a8a97a0a8c5c75ef236889b9480d0c2f
叶月汶 <yeyuewen@seeiner.com>
2017-05-22 13:33:12 +0800

    pwa

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', function (event) {
   if (/offline-data$/.test(event.request.url)) {
-    event.responseWith(
+    event.respondWith(
       new Response('这是一条来自service worker的响应信息')
     )
   }
